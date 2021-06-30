6185e05086f1e7bb856c871959d4c87ff3fac1ff
Mark Anderson <markanderson8162@gmail.com>
2021-06-30 09:44:46 -0500

    finished name list exercise

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var gameNames = new List<string> { "The Legend of Zelda: A Link to the Past", "Portal", "Rocket League"};
+			//string[] gameNames = { "The Legend of Zelda: A Link to the Past", "Portal", "Rocket League" };
+			//string gameOne = gameNames[0];
+			//int gameOneLength = gameNames[0].Length;
+
+			//string gameTwo = gameNames[1];
+			//int gameTwoLength = gameNames[1].Length;
+			//int gameThreeLength = gameNames[2].Length;
+
+			//int[] gameNamesLength = { gameOneLength, gameTwoLength, gameThreeLength };
+
+			var nameOrder = gameNames.OrderBy(name => name.Length);
+			foreach(string name in nameOrder)
+			{
+				Console.WriteLine(name);
+			}
+
+		}
+	}
+}
